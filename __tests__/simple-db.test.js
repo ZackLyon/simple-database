612c8380d7211bbc03384b0d2e931d93091c96a4
Zack Lyon <zack.j.lyon@gmail.com>
2022-02-14 12:58:39 -0800

    Add get by id feature

--- __tests__/simple-db.test.js
+++ __tests__/simple-db.test.js
@@ -1,19 +1,35 @@
 const fs = require('fs/promises');
+const { nanoid } = require('nanoid');
 const path = require('path');
+const SimpleDb = require('../lib/simple-db.js');
 
 const { CI, HOME } = process.env;
 const BASE_DIR = CI ? HOME : __dirname;
 const TEST_DIR = path.join(BASE_DIR, 'test-dir');
 
-describe('simple database', () => {
+const id = nanoid(5);
+const testObj = { id, message: 'this is where the magic happens' };
+const testJSONobj = JSON.stringify(testObj);
+let newDB;
 
+describe('simple database', () => {
   beforeEach(async () => {
     await fs.rm(TEST_DIR, { force: true, recursive: true });
     await fs.mkdir(TEST_DIR, { recursive: true });
+    newDB = new SimpleDb(TEST_DIR);
   });
 
-  it('needs a first test...', async () => {
+  it('should get a file by id', async () => {
+    const filePath = path.join(TEST_DIR, `${id}.json`);
+    await fs.writeFile(filePath, testJSONobj);
 
+    const file = await newDB.get(id);
+    expect(file).toEqual(testObj);
   });
 
+  it('should return null if get(id) does not find a file', async () => {
+    const file = await newDB.get('fakeId');
+
+    expect(file).toBeNull();
+  });
 });
